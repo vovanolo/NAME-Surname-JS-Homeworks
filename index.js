64a60dc4dd3ac54a76416d63ff83e0403bc841c1
vovanolo <39496114+vovanolo@users.noreply.github.com>
2022-05-19 19:45:07 +0300

    homework1

---
+++ index.js
@@ -0,0 +1,20 @@
+// const PI = 2000;
+
+// PI = 4000; 
+
+
+// let myNewveriableSogood
+
+// const user = {
+//     name: 'Volodya',
+//     age: 205
+// }
+
+// console.log(user);
+
+// alert("hello js");
+
+let age = prompt("what is your age", "18");
+
+console.log(age);
+
